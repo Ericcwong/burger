2304dbd7c600ff25722b51bc001c04a9e4759356
Eric Wong <ericwong12@live.com>
2020-02-23 15:28:20 -0700

    Created connection to mysql database created express server

---
+++ assets/js/connection.js
@@ -0,0 +1,25 @@
+//Connection.js creates the connection to the MySQL database
+
+
+//dependencies
+const mysql = require("mysql");
+
+
+//creates a connection to the mysql database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: "burger_db"
+});
+
+connection.connect(function(err){
+    if(err){
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ assets/js/server.js
@@ -0,0 +1,26 @@
+//Dependencies
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+
+//Calling express function as app
+//setting PORT to whatever heroku calls if not 9000 localhost:
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+//Middleware handling all the data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
+//Starts the server
+app.listen(PORT, function(){
+    console.log("Server listening on: http://localhost: " + PORT);
+})
