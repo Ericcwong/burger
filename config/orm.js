3bb707e15bb2e70fae0587613b6113937871102e
Eric Wong <ericwong12@live.com>
2020-02-24 14:33:58 -0700

    orm.js has the mysql functions but updateOne might be wrong

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,27 @@
 const connection = require("./connection");
 
-
-
-
-
-
-
+let orm = {
+   selectAll: function(whatToSelect, table){
+       let selectAllQuery = "SELECT ?? FROM ??";
+       connection.query(selectAllQuery, [whatToSelect,table],function(err,res){
+           if(err) throw err;
+           console.log(res);
+       });
+   },
+   insertOne: function(whatTable,value0,value1){
+       let insertQuery = "INSERT INTO ?? (??, ??) VALUES (??, ??)";
+       connection.query(insertQuery, [whatTable, value0, value1, value0, value1],function(err,res){
+           if(err) throw err;
+           console.log(res);
+       });
+   },
+   updateOne: function(whatTable, oldName, updatedName,oldBoolean,updateBoolean, id ){
+       let updateQuery = "UPDATE ?? SET ?? = ??, ?? = ?? WHERE ?? = ?";
+       connection.query(updateQuery,[whatTable,oldName,updatedName,oldBoolean,updateBoolean,id,id],function(err,res){
+           if(err) throw err;
+           console.log(res);
+       });
+   }
+};
 
 module.exports = orm;
